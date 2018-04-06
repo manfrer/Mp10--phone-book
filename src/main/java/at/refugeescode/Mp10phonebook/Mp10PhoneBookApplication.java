32d9adafb88ffa8e1c42a127f636a39b0e8af7a7
nana80802 <miss_nisreen2000@yahoo.com>
2018-04-06 12:52:49 +0200

    set the spring structure

---
+++ src/main/java/at/refugeescode/Mp10phonebook/Mp10PhoneBookApplication.java
@@ -0,0 +1,12 @@
+package at.refugeescode.Mp10phonebook;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Mp10PhoneBookApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Mp10PhoneBookApplication.class, args);
+	}
+}

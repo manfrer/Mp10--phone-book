310f3d24085f0cb797688b7df7eb9fa08dca41f5
nana80802 <miss_nisreen2000@yahoo.com>
2018-04-08 18:33:43 +0200

    finished with post 10

--- src/main/java/at/refugeescode/Mp10phonebook/endpoint/PersonEndPoint.java
+++ src/main/java/at/refugeescode/Mp10phonebook/endpoint/PersonEndPoint.java
@@ -5,6 +5,7 @@ import at.refugeescode.Mp10phonebook.persistence.repository.PersonRepository;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/contacts")
@@ -14,6 +15,14 @@ public class PersonEndPoint {
     public PersonEndPoint(PersonRepository personRepository) {
         this.personRepository = personRepository;
     }
+    @GetMapping("/{id}")
+    Person showOneById(@PathVariable Long id){
+        Optional<Person> optionalPerson = personRepository.findById(id);
+        if (optionalPerson.isPresent())
+            return optionalPerson.get();
+        return null;
+    }
+
     @GetMapping
     List<Person> showAll(){
         return personRepository.findAll();
@@ -22,5 +31,11 @@ public class PersonEndPoint {
     @PostMapping
     Person save(@RequestBody Person newPerson){
         return personRepository.save(newPerson);
+    }
+
+    @DeleteMapping("/{id}")
+    String deleteOneById(@PathVariable Long id){
+        personRepository.deleteById(id);
+        return "the contact has been deleted";
     }
 }
